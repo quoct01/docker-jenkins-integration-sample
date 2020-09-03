42968a7a5e82bb8b156832a44f05ff218d7643d4
quoct01 <qtangbui@gmail.com>
2020-09-03 18:02:01 +0700

    update lan thu 3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Tang");
+        System.out.println("Hello from Tang, xin chao");
     }
 }
