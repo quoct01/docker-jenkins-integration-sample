a0f45154d7fa14f87d555e6b426e42e6331eaf0c
quoct01 <qtangbui@gmail.com>
2020-09-03 18:57:27 +0700

    update file lan thu 6

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Tang, xin chao");
+        System.out.println("Hello from Tang, xin chao lan 4");
     }
 }
